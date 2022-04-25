f807f96e57ad24480a815b55ed280459ceae84dc
gillsukhman209 <gillsukhman209@gmail.com>
2022-04-25 11:47:20 -0700

    Display working, calculations working, going to work on the clear button

--- app.js
+++ app.js
@@ -1,71 +1,97 @@
 const btns = document.querySelectorAll(".btn")
+const display = document.querySelector(".display")
 let operation = 0;
-let currentNumber = 0
-let previousNumber;
+let currentNumber = ""
+let previousNumber = ""
+let displayText = ""
 
 btns.forEach((btn) => {
 
 
     btn.addEventListener("click", (e) => {
+        if (e.target.textContent != "=") {
+            displayText = display.textContent + e.target.textContent
+            display.textContent = displayText
+        }
+
         switch (e.target.textContent) {
             case "+":
                 operation = 1;
-                console.log("operation 1")
+
                 break;
             case "-":
                 operation = 2;
-                console.log("operation 2")
+
                 break;
 
             case "/":
-                operation = 1;
-                console.log("operation 3")
+                operation = 3;
+
                 break;
             case "X":
-                operation = 2;
-                console.log("operation 4")
+                operation = 4;
+                break;
+            case "=":
+                calculate(operation, previousNumber, currentNumber)
+                previousNumber = ""
+                currentNumber = ""
+                break;
+            default:
+
+                if (previousNumber == "") {
+                    previousNumber = parseInt(e.target.textContent)
+
+                } else {
+                    currentNumber = parseInt(e.target.textContent)
+
+                }
+
                 break;
 
+
+
+
+
         }
-        if (!e.target.textContent in ["+", "-", "/", "X"]) {
-            currentNumber = parseInt(e.target.textContent)
-            console.log("Current number " + currentNumber)
-        }
+
     })
 })
 
 function calculate(operation, num1, num2) {
-    if (operation == 0) {
-        sum(num1, num2)
-    }
+
     switch (operation) {
         case 1:
-            sum(num1, num2);
+            updateDisplay(sum(num1, num2))
+
             break;
         case 2:
-            sub(num1, num2);
+            updateDisplay(sub(num1, num2))
             break
         case 3:
-            div(num1, num2);
+            updateDisplay(div(num1, num2))
             break;
         case 4:
-            mul(num1, num2);
+            updateDisplay(mul(num1, num2))
             break
     }
+}
+
+function updateDisplay(text) {
+    display.textContent = text
 }
 
 function sum(num1, num2) {
-    return num1 + num2
+    return (num1 + num2)
 }
 
 function sub(num1, num2) {
-    return num1 - num2
+    return (num1 - num2)
 }
 
 function div(num1, num2) {
-    return num1 / num2
+    return (num1 / num2)
 }
 
 function mul(num1, num2) {
-    return num1 * num2
+    return (num1 * num2)
 }
